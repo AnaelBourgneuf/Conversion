cf73380a0d2226488620d60f694341c233441305
juliopopo1234 <ikaratchi@gmail.com>
2018-02-05 10:08:29 +0100

    envoie des fichiers de base

---
+++ Conversion.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SuperConversions;
+
+import java.awt.GraphicsConfiguration;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Dragonlord
+ */
+public class Conversion extends JFrame{
+
+    public static void main(String[] args) {
+        JFrame fenetre = new JFrame();
+        fenetre.setTitle("Conversion");
+        fenetre.setSize(800,600);
+        fenetre.setLocationRelativeTo(null);
+        fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        fenetre.setVisible(true);
+    }
+    
+}
