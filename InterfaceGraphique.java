ac090da7f62f079b0051393eb4867148a607f784
juliopopo1234 <ikaratchi@gmail.com>
2018-02-05 12:09:22 +0100

    MAJ interfaceG

--- InterfaceGraphique.java
+++ InterfaceGraphique.java
@@ -5,20 +5,61 @@
  */
 package SuperConversions;
 
+import java.awt.Dimension;
 import java.awt.GraphicsConfiguration;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 /**
  *
  * @author Anael
  */
-public class InterfaceGraphique {
-    public static void main(String[] args) {
-        JFrame fenetre = new JFrame();
-        fenetre.setTitle("Conversion");
-        fenetre.setSize(800,600);
-        fenetre.setLocationRelativeTo(null);
-        fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        fenetre.setVisible(true);
+public class InterfaceGraphique extends JFrame {
+    public static JComboBox combo = new JComboBox();
+    public static JLabel label = new JLabel("Que voulez vous convertir ?");
+    public static JPanel container = new JPanel();
+    public static JButton bouton = new JButton("Valider");
+
+
+    public InterfaceGraphique(){
+        setTitle("Conversion");
+        setSize(800,600);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+       
+        container.setBackground(Color.white);
+        container.setLayout(new BorderLayout());
+   
+        
+        combo.setPreferredSize(new Dimension(100, 20));
+        combo.addItem("Heure");
+        combo.addItem("Température");
+        combo.addItem("Devises");
+        
+        JPanel top = new JPanel();
+        top.add(label);
+        top.add(combo);
+        container.add(top, BorderLayout.NORTH);
+        
+        JPanel bot = new JPanel();
+        bot.add(bouton);
+        container.add(bot, BorderLayout.SOUTH);
+        
+        setContentPane(container);
+        setVisible(true);
     }
+    
+    
+    public static void main(String[] args) {
+        new InterfaceGraphique();
+    }
+
+    
 }
