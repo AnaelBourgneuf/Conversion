43e819cad507f304a4a44c13a274007dd101c843
Bourgneuf-Monclair Anael <anael2011@hotmail.fr>
2018-02-08 11:04:13 +0100

    devises de api ok, erreurs d'entrée gérées, renvoie le ration en fonction de deux devises

--- Devise.java
+++ Devise.java
@@ -12,6 +12,7 @@ import com.mashape.unirest.request.GetRequest;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
@@ -52,25 +53,47 @@ public class Devise {
         return api;
     }
     
-    public static void loadRateFromJSON(String from, String to) throws IOException, ParseException, JSONException{
+    public static float loadRateFromJSON(String from, String to) throws IOException, ParseException, JSONException{
         JSONArray json = (JSONArray) readJsonFromUrl("https://api.devises.zone/v1/full/"+from+"/json?key=51%7CXyXBB~swhTX%5EkguJkHjSj~mZ0WA4c9rt").getJSONObject("result").get("conversion");
         //JSONArray jsonarray = json.toJSONArray(new JSONArray());
         //ArrayList array = new ArrayList();
         String monnaie = "";
+        JSONObject dic = null;
         int i = 0;
         while (!monnaie.equals(to)){
-            JSONArray array = json.getJSONArray(i);
-            monnaie = array.
+            dic = (JSONObject) json.get(i);
+            monnaie = (String) dic.get("to");
             i++;
         }
+        double rate = (double) dic.get("rate");
         
-        //while (!monnaie.equals(json.get("result")))
-        System.out.println(json.get(0));
+        //System.out.println(rate);
+        return (float) rate;
+    }
+    
+    public static float loadRate(String from, String to) throws IOException, ParseException, JSONException{
+        Hashtable dic = new Hashtable();
+        dic.put("Dollard des Etats Unis","USD");dic.put("Euro","EUR");dic.put("Livre Britanique","GBP");dic.put("Roupie Indienne","INR");dic.put("Dollard Australien","AUD");dic.put("Dollard Canadien","CAD");dic.put("Dollard de Singapour","SGD");dic.put("Yen Japonais","JPY");dic.put("Yuan ou renminbi chinois","CNY");
+        if (dic.containsKey(from)){
+            from = (String) dic.get(from);
+        }
+        else {
+            System.out.println("Monnaie de départ non prise en compte.");
+            return 0;
+        }
+        if (dic.containsKey(to)){
+            to = (String) dic.get(to);
+        }
+        else {
+            System.out.println("Monnaie d'arrivée non prise en compte.");
+            return 0;
+        }
+        return loadRateFromJSON(from, to);
     }
     
     public static void main(String[] args){
         try {
-            loadRateFromJSON("EUR", "USD");
+            System.out.println(loadRate("Euro", "Dollard des Etats Unis"));
         } catch (IOException | ParseException | JSONException ex) {
             Logger.getLogger(Devise.class.getName()).log(Level.SEVERE, null, ex);
         }
