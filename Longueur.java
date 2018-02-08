2710207e897c2d605a1050d56b6e31a9f84e7d41
Bourgneuf-Monclair Anael <anael2011@hotmail.fr>
2018-02-08 12:35:56 +0100

    debuts pour prendre en compte longueurs et volumes

---
+++ Longueur.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SuperConversions;
+
+/**
+ *
+ * @author Anael
+ */
+public class Longueur {
+    private double millimeterToMeter = 0.001;
+    private double centimeterToMeter = 0.01;
+    private double decimeterToMeter = 0.1;
+    private float meterToMeter = 1;
+    private float kilometerToMeter = 1000;
+    private double footToMeter = 0.3048;
+    private double inchToMeter = 0.0254;
+    private double mileToMeter = 1609.344;
+    private double yardToMeter = 0.9144;
+}
---
+++ Volume.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SuperConversions;
+
+/**
+ *
+ * @author Anael
+ */
+public class Volume {
+    private double cubicFootToCubicMetter = 0.02831685;
+    private double cubicInchToCubicMetter = 0.00001638706;
+    private long cubicMileToCubicMetter = 4168182*1000;
+    private double cubicYardToCubicMetter = 0.7645549;
+    private double cupToCubicMetter = 0.0002365882;
+    private double imperialGallonToCubicMetter = 0.00454609;
+    private double usGallonToCubicMetter = 0.003785412;
+    private double milliliterToCubicMetter = 0.000001;
+    private double literToCubicMetter = 0.001;
+}
